9f2a244e535b634c3873509b4620d829cbef69a6
Yana Sirina <yanasirina0@yandex.ru>
2023-01-06 12:55:41 +0500

    константы для клавиатур

--- consts.py
+++ consts.py
@@ -1,1 +1,13 @@
 API_CATS_URL: str = 'https://aws.random.cat/meow'
+
+POSSIBLE_BUTTONS = ['btn_1', 'btn_2', 'btn_3', 'btn_4', 'btn_5', 'btn_6', 'btn_7', 'btn_8', 'btn_9']
+
+BUTTONS: dict[str, str] = {'btn_1': '1',
+                           'btn_2': '2',
+                           'btn_3': '3',
+                           'btn_4': '4',
+                           'btn_5': '5',
+                           'btn_6': '6',
+                           'btn_7': '7',
+                           'btn_8': '8',
+                           'btn_9': '9'}
